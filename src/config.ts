d62c525aa70628aebb245c1df15fe1dd5d407e23
Eugene Tsang <eugene.tsang@gmail.com>
2018-12-10 13:55:24 +1300

    Use standardjs

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 
 const config:IEnvConfig = {
-    sandbox: {
-        ORACLE_URL: 'https://s3-ap-southeast-2.amazonaws.com/chainops-oracle-production-history-store/',
-        SUBSCRIPTIONS_ENDPOINT: 'https://vzofrqtt33.execute-api.ap-southeast-2.amazonaws.com/mainnet',
-    },
-    production: {
-        ORACLE_URL: 'https://s3-ap-southeast-2.amazonaws.com/chainops-oracle-production-history-store/',
-        SUBSCRIPTIONS_ENDPOINT: 'https://pxzea0vim3.execute-api.ap-southeast-2.amazonaws.com/mainnet',
-    },
+  sandbox: {
+    ORACLE_URL: 'https://s3-ap-southeast-2.amazonaws.com/chainops-oracle-production-history-store/',
+    SUBSCRIPTIONS_ENDPOINT: 'https://vzofrqtt33.execute-api.ap-southeast-2.amazonaws.com/mainnet'
+  },
+  production: {
+    ORACLE_URL: 'https://s3-ap-southeast-2.amazonaws.com/chainops-oracle-production-history-store/',
+    SUBSCRIPTIONS_ENDPOINT: 'https://pxzea0vim3.execute-api.ap-southeast-2.amazonaws.com/mainnet'
+  }
 }
 
 export interface IConfig {
@@ -19,4 +19,4 @@ interface IEnvConfig {
     [env: string]: IConfig
 }
 
-export default config;
+export default config
--- src/index.ts
+++ src/index.ts
@@ -1,104 +1,103 @@
-import aws4 from 'aws4';
-import axios from 'axios';
-import {URL} from 'url';
-import {SharedIniFileCredentials, Config} from 'aws-sdk';
+import aws4 from 'aws4'
+import axios from 'axios'
+import { URL } from 'url'
+import { SharedIniFileCredentials, Config } from 'aws-sdk'
 
-import config, { IConfig } from './config';
+import config, { IConfig } from './config'
 
 export class ChainOps {
     awsConfig: Config;
     config: IConfig;
 
     constructor (env: string | IConfig) {
-        this.awsConfig = new Config();
-        this.awsConfig.credentials = new SharedIniFileCredentials();
+      this.awsConfig = new Config()
+      this.awsConfig.credentials = new SharedIniFileCredentials()
 
-        if (typeof env === 'string') {
-            console.log('Setting config from env:', env);
-            this.config = config[env];
-        }else{
-            console.log('Setting config from object:', env);
-            this.config = env;
-        }
+      if (typeof env === 'string') {
+        console.log('Setting config from env:', env)
+        this.config = config[env]
+      } else {
+        console.log('Setting config from object:', env)
+        this.config = env
+      }
     }
 
     async getGasPrice (blockNumber?: number) {
-        const file = `${blockNumber || 'latest'}.json`;
+      const file = `${blockNumber || 'latest'}.json`
 
-        const response = await axios.request({
-            baseURL: this.config.ORACLE_URL,
-            url: file
-        });
+      const response = await axios.request({
+        baseURL: this.config.ORACLE_URL,
+        url: file
+      })
 
-        try {
-            return response.data.analysis;
-        }catch(err) {
-            throw new Error(`Could not retrieve gas prices for block: ${blockNumber || 'latest'}`);
-        }
+      try {
+        return response.data.analysis
+      } catch (err) {
+        throw new Error(`Could not retrieve gas prices for block: ${blockNumber || 'latest'}`)
+      }
     }
-    
+
     async subscribe (subConfig: any) {
-        const url = new URL(this.config.SUBSCRIPTIONS_ENDPOINT + '/subscription');
-        
-        //@ts-ignore
-        await this.awsConfig.credentials.getPromise();
-        if (!this.awsConfig.credentials) throw new Error('AWS creds not set'); 
+      const url = new URL(this.config.SUBSCRIPTIONS_ENDPOINT + '/subscription')
 
-        const request = aws4.sign({
-            host: url.host,
-            url: url.href,
-            method: 'PUT',
-            path: `${url.pathname}${url.search}`,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(subConfig)
-        }, {
-            secretAccessKey: this.awsConfig.credentials.secretAccessKey,
-            accessKeyId: this.awsConfig.credentials.accessKeyId,
-            sessionToken: this.awsConfig.credentials.sessionToken
-        });
+      // @ts-ignore
+      await this.awsConfig.credentials.getPromise()
+      if (!this.awsConfig.credentials) throw new Error('AWS creds not set')
 
-        const reqConfig = {
-            method: request.method,
-            url: request.url,
-            headers: request.headers,
-            data: JSON.stringify(subConfig),
-        };
+      const request = aws4.sign({
+        host: url.host,
+        url: url.href,
+        method: 'PUT',
+        path: `${url.pathname}${url.search}`,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(subConfig)
+      }, {
+        secretAccessKey: this.awsConfig.credentials.secretAccessKey,
+        accessKeyId: this.awsConfig.credentials.accessKeyId,
+        sessionToken: this.awsConfig.credentials.sessionToken
+      })
 
-        const response = await axios.request(reqConfig);
-        return response.data;
+      const reqConfig = {
+        method: request.method,
+        url: request.url,
+        headers: request.headers,
+        data: JSON.stringify(subConfig)
+      }
+
+      const response = await axios.request(reqConfig)
+      return response.data
     }
 
     async unsubscribe (subscriptionId: string) {
-        const url = new URL(this.config.SUBSCRIPTIONS_ENDPOINT + '/subscription/' + subscriptionId);
+      const url = new URL(this.config.SUBSCRIPTIONS_ENDPOINT + '/subscription/' + subscriptionId)
 
-        //@ts-ignore
-        await this.awsConfig.credentials.getPromise();
-        if (!this.awsConfig.credentials) throw new Error('AWS creds not set'); 
+      // @ts-ignore
+      await this.awsConfig.credentials.getPromise()
+      if (!this.awsConfig.credentials) throw new Error('AWS creds not set')
 
-        const request = aws4.sign({
-            host: url.host,
-            url: url.href,
-            method: 'DELETE',
-            path: `${url.pathname}${url.search}`,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }, {
-            secretAccessKey: this.awsConfig.credentials.secretAccessKey,
-            accessKeyId: this.awsConfig.credentials.accessKeyId,
-            sessionToken: this.awsConfig.credentials.sessionToken
-        });
+      const request = aws4.sign({
+        host: url.host,
+        url: url.href,
+        method: 'DELETE',
+        path: `${url.pathname}${url.search}`,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }, {
+        secretAccessKey: this.awsConfig.credentials.secretAccessKey,
+        accessKeyId: this.awsConfig.credentials.accessKeyId,
+        sessionToken: this.awsConfig.credentials.sessionToken
+      })
 
-        const reqConfig = {
-            method: request.method,
-            url: request.url,
-            headers: request.headers
-        };
+      const reqConfig = {
+        method: request.method,
+        url: request.url,
+        headers: request.headers
+      }
 
-        const response = await axios.request(reqConfig);
-        return response.data;
+      const response = await axios.request(reqConfig)
+      return response.data
     }
-
 }
