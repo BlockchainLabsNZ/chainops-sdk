8fc4733f5f3466a47c7ad9e537b4adc719a27b87
Eugene Tsang <eugene.tsang@gmail.com>
2018-12-18 16:27:01 +1300

    Update config endpoints

--- dist/config.js
+++ dist/config.js
@@ -1,13 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const config = {
-    sandbox_kovan: {
+    kovan: {
         ORACLE_URL: 'https://s3-ap-southeast-2.amazonaws.com/chainops-oracle-production-history-store/',
         SUBSCRIPTIONS_ENDPOINT: 'https://374dz4opt3.execute-api.ap-southeast-2.amazonaws.com/kovan'
-    },
-    sandbox_mainnet: {
-        ORACLE_URL: 'https://s3-ap-southeast-2.amazonaws.com/chainops-oracle-production-history-store/',
-        SUBSCRIPTIONS_ENDPOINT: 'https://vzofrqtt33.execute-api.ap-southeast-2.amazonaws.com/mainnet'
     },
     production: {
         ORACLE_URL: 'https://s3-ap-southeast-2.amazonaws.com/chainops-oracle-production-history-store/',
--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,8 @@
 
 const config:IEnvConfig = {
-  sandbox_kovan: {
+  kovan: {
     ORACLE_URL: 'https://s3-ap-southeast-2.amazonaws.com/chainops-oracle-production-history-store/',
     SUBSCRIPTIONS_ENDPOINT: 'https://374dz4opt3.execute-api.ap-southeast-2.amazonaws.com/kovan'
-  },
-  sandbox_mainnet: {
-    ORACLE_URL: 'https://s3-ap-southeast-2.amazonaws.com/chainops-oracle-production-history-store/',
-    SUBSCRIPTIONS_ENDPOINT: 'https://vzofrqtt33.execute-api.ap-southeast-2.amazonaws.com/mainnet'
   },
   production: {
     ORACLE_URL: 'https://s3-ap-southeast-2.amazonaws.com/chainops-oracle-production-history-store/',
