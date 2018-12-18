63172da635695aa98e788e034cdd19fadf240687
Eugene Tsang <eugene.tsang@gmail.com>
2018-12-18 16:30:27 +1300

    Update config endpoints

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ const config:IEnvConfig = {
     ORACLE_URL: 'https://s3-ap-southeast-2.amazonaws.com/chainops-oracle-production-history-store/',
     SUBSCRIPTIONS_ENDPOINT: 'https://374dz4opt3.execute-api.ap-southeast-2.amazonaws.com/kovan'
   },
-  production: {
+  mainnet: {
     ORACLE_URL: 'https://s3-ap-southeast-2.amazonaws.com/chainops-oracle-production-history-store/',
     SUBSCRIPTIONS_ENDPOINT: 'https://pxzea0vim3.execute-api.ap-southeast-2.amazonaws.com/mainnet'
   }
